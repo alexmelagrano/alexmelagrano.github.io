04ae19a324b1119d261670d6c48f6ef4c84daa40
Alex Melagrano <alexmelagrano@gmail.com>
2017-01-19 00:03:59 -0500

    attempt #3 on CORS fix

--- app/services/forecast.js
+++ app/services/forecast.js
@@ -5,46 +5,50 @@ angular.module('DarkSky')
 
     // Calls the Dark Sky API, returns the resulting JSON
     .factory('forecast', ['$http', function($http) {
-        var url = 'https://api.darksky.net/forecast/d545dfd43b28ee98f767b4eaf6b7b454/42.3601,-71.0589';
+        var url = 'https://thingproxy.freeboard.io/fetch/https://api.darksky.net/forecast/d545dfd43b28ee98f767b4eaf6b7b454/42.3601,-71.0589';
         var testUrl = 'https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json';
 
         var forecast = {};
 
+        console.log('Going to this url:');
+        console.log(url);
+
         forecast.getWeather = function(callback) {
             console.log('Began the API call');
-            // $http.get(url)
-            //     .success(function(data) {
-            //         callback(data);
-            //         console.log("Returned the Dark Sky data, seen here:");
-            //         console.log(data);
-            // })
-            //     .error(function(err) {
-            //         callback(err);
-            //         console.log('Something fucked up in the API call...');
-            //     });
 
-            $http({
-                url: url,
-                method: 'GET',
-                contentType: 'text/plain',
-                xhrFields: {
-                    withCredentials: true
-                },
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Access-Control-Allow-Origin': 'http://alexmelagrano.github.io/'
-                },
-
-                success: function(data) {
+            $http.get(url)
+                .success(function(data) {
                     callback(data);
-                    console.log("Retrieved the Dark Sky data, seen here:");
+                    console.log("Returned the Dark Sky data, seen here:");
                     console.log(data);
-                },
-                error: function(err) {
+                })
+                .error(function(err) {
                     callback(err);
-                    console.log("Something went wrong...");
-                }
-            });
+                    console.log('Something fucked up in the API call...');
+                });
+
+            // $http({
+            //     url: url,
+            //     method: 'GET',
+            //     contentType: 'text/plain',
+            //     xhrFields: {
+            //         withCredentials: true
+            //     },
+            //     headers: {
+            //         'Content-Type': 'application/x-www-form-urlencoded',
+            //         'Access-Control-Allow-Origin': 'http://localhost:63342'
+            //     },
+            //
+            //     success: function(data) {
+            //         callback(data);
+            //         console.log("Retrieved the Dark Sky data, seen here:");
+            //         console.log(data);
+            //     },
+            //     error: function(err) {
+            //         callback(err);
+            //         console.log("Something went wrong...");
+            //     }
+            // });
 
             // var setHeader = function(xhr) {
             //     xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
